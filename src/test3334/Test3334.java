ad658493449716effa60df3cc34f29369f9efc81
Johann Weiser <johann.weiser@aon.at>
2017-03-15 20:14:54 +0100

    First version.

---
+++ src/test3334/Test3334.java
@@ -0,0 +1,17 @@
+package test3334;
+
+public class Test3334 {
+
+	public static void test3334() {
+		System.out.println("Test3334 erfolgreich gestartet.");
+	}
+	
+	public static void test3334Finish() {
+		System.out.println("Test3334 erfolgreich beendet.");
+	}
+	public static void main(String[] args) {
+		test3334();
+		test3334Finish();
+	}
+
+}
