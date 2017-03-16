cb4f57f5c05bff39d2c93c5165300e0838b0d2a0
ntesanovic <ntesanovic@student.tgm.ac.at>
2017-03-16 11:20:54 +0100

    Masterbranch geändert
    Smileys hinzugefügt

--- src/test3334/Test3334.java
+++ src/test3334/Test3334.java
@@ -3,11 +3,11 @@ package test3334;
 public class Test3334 {
 
 	public static void test3334() {
-		System.out.println("Test3334 erfolgreich gestartet!");
+		System.out.println("Test3334 erfolgreich gestartet! :)");
 	}
 	
 	public static void test3334Finish() {
-		System.out.println("Test3334 erfolgreich beendet!");
+		System.out.println("Test3334 erfolgreich beendet! :(");
 	}
 	public static void main(String[] args) {
 		for (int i = 0;i<3;i++) {
